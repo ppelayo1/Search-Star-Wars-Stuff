288ee7a29679598d71673561c6325c8be94801c1
Patrick Pelayo <patrickp@patrickp.tech>
2020-11-22 08:47:44 -0800

    finished the hint section with classes

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,7 @@
 //This object will store all plugin JS wide variables
 let PPSTARWARSCONST = {
-    WIDGET_CLASSNAME: 'starWarsWidget', //this is the class name of the widget in widget.php
-    OUTPUT_CLASSNAME: 'starWarsOutput'  //The class name for each output item in the list
+    WIDGET_CLASSNAME: 'starWarsWidget', //This is the class name of the widget in widget.php
+    OUTPUT_CLASSNAME: 'starWarsOutput', //The class name for each output item in the list
+    ACTION_AJAX_HINT: 'patrickp_star_wars_query_hint', //The wordpress action for the ajax hint call
+    ACTION_AJAX_SUBMIT: 'patrickp_star_wars_query_submit' //The wordpress action for the ajax submit call
 };
--- src/js/widget.js
+++ src/js/widget.js
@@ -11,75 +11,76 @@ jQuery(document).ready(()=>{
         //initialize all variables and set up event handlers
         initialize(){
             this.widgetWrapper = jQuery('.' + PPSTARWARSCONST.WIDGET_CLASSNAME).children('form');
+            
+            this.setUpHintHandler();
         }
         
         setUpHintHandler(){
+            let input = jQuery(this.widgetWrapper).children('input');
+            let delay = 0; //delay in ms for the auto complete to appear
+            let minLength = 2; //min length of string input before generating auto complete list
             
-        }
-    }
-    
-    
-    //all the forms assigned to a variable
-    let form = jQuery('.' + PPSTARWARSCONST.WIDGET_CLASSNAME).children('form');
-    
-    //listen for input to the search bar
-    setUpHintHandler();
-    
-    //perform a search attempt when form submitted
-    form.submit(submit);
-    
-    //functions
-    
-    //This function sets up the auto complete hint handler for each star wars search widget
-    function setUpHintHandler(){
-        let input = jQuery(form).children('input');
-        
-        jQuery(input).each(function () {
-            let searchArray = []; //needed to store the autocomplete data for the keyup event handler
+            jQuery(input).each(function () {
+                let searchArray = []; //needed to store the autocomplete data for the keyup event handler
+
+                jQuery(this).autocomplete({
+                    delay:delay,
+                    minLength:minLength,
+                    source:function(request,response) {source(request,response,searchArray);},
+                    select:(event,ui)=>{autoCompleteSubmit(this,ui.item.value);} //perform a search query if user clicks on a name
+                }).keyup(function (e) {keyUp.call(this,e,searchArray);}); 
+            })
             
-            jQuery(this).autocomplete({
-                delay:0,
-                minLength:2,
-                source:(request,response)=>{
-                    let data = {
-                    'action': 'patrickp_star_wars_query_hint',
-                    'search_term': request.term  
+            //these functions are used in the jQuery autocomplete
+                
+            //this performs the ajax call,sets the response list for the auto complete, and returns the list
+            function source(request,response,searchArray){
+                //empty searchArray
+                searchArray = [];
+                
+                let data = {
+                        'action': PPSTARWARSCONST.ACTION_AJAX_HINT,
+                        'search_term': request.term  
                     };
 
                     jQuery.get(ajax_object.ajax_url,data,function(responseData){
-                        let source = [];//the source that the auto complete will use
                         responseData = JSON.parse(responseData);
-                        
+
                         //need to build an array of names from the returned get data
                         for(let i = 0; i < responseData.length;i++){
-                            source.push(responseData[i].name);
+                            searchArray.push(responseData[i].name);
                         }
-                        response(source);
-                        searchArray = source;
+                        response(searchArray);
                     });
-                },
-                select:(event,ui)=>{
-                    autoCompleteSubmit(this,ui.item.value); //perform a search query if user clicks on a name
-                }
-            }).keyup(function (e) {
+            }
+            
+            //handles the keyup event, closes the auto complete when enter key is pressed and a valid input is submited
+            function keyUp(e,searchArray){
                 //variables
                 let tempArray = []; //temp holds the autocomplete data in lower case
                 let input = jQuery(this).val(); //just the input held in the text box
+                
                 input = input.toLowerCase(); //set that input lower case
                 
                 //set the autocomplete data to lower case
                 for(let i = 0; i < searchArray.length;i++){
                     tempArray.push(searchArray[i].toLowerCase());
                 }
-                
+
                 //close auto complete if enter key pressed and a search term is matched
                 if(e.keyCode == 13 && (tempArray.indexOf(input) >= 0)){
                     jQuery(this).autocomplete('close');
                 }
-            });
-        })
-        
+            }
+        }
     }
+    
+    new StarWarsAjax();
+    //all the forms assigned to a variable
+    let form = jQuery('.' + PPSTARWARSCONST.WIDGET_CLASSNAME).children('form');
+    
+    //perform a search attempt when form submitted
+    form.submit(submit);
     
     //handles the query when the user presses enter or presses the submit button
     function submit(){
